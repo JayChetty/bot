fac21f651c239f9a18b85723bd3a313a95465345
Jay Chetty <jaypaulchetty@gmail.com>
2017-06-12 15:49:45 +0100

    setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CodeMirror from 'react-codemirror';
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/mode/javascript/javascript'
 
+// const code = "Head head = new Head();Backpack backPack = new BackPack();Bot botty = new Bot(Head head, BackPack backpack);"
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        <div className="Feedback">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Instructions">
+          <div>Head head = new Head();</div>
+          <div>Backpack backPack = new BackPack();</div>
+          <div>Bot botty = new Bot(Head head, BackPack backpack);</div>
+          <div> botty.headAction() </div>
+          <div> botty.backPackAction() </div>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
+//
+// <div>Head head = new Head();</div>
+// <div>Backpack backPack = new BackPack();</div>
+// <div>Bot botty = new Bot(Head head, BackPack backpack);</div>
